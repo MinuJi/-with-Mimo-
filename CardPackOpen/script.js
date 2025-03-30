87b4785a6ceeb245958cf7672e44183e4425b30a
MinuJi <minwooji0715@gmail.com>
2025-03-30 22:59:11 +0900

    Add files via upload
    fc25에 관심이 많은 나는 내가 배운 내용들을 종합해서 카드 오픈하는 웹을 만들었다.

---
+++ CardPackOpen/script.js
@@ -0,0 +1,56 @@
+function press() {
+    const el = document.querySelector("#who");
+    const paragraph = document.querySelector("#what"); 
+    el.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSndPW9Mql9e2iKKUi8ObVVZzOGvaXzVGdjpg&s";
+    paragraph.textContent = "과연 누가 나올까요?";
+    }
+   
+    function practiceBack() {
+    const el = document.querySelector("#who");
+    const paragraph = document.querySelector("#what"); 
+    el.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHtNhhArQt2gDqVVtc_K_n-3OhMrfBLjyIHg&s";
+    paragraph.textContent = "";
+    }
+   
+   
+   function firstPack() {
+    const el = document.querySelector("#first");
+    const paragraph = document.querySelector("#ruud"); 
+    el.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhqcM6eaq2FwOPftU25I_euNA4rjvGjUQDSQ&s";
+    paragraph.textContent = "Ruud van Nistelrooy";
+    }
+   
+    function firstBack() {
+    const el = document.querySelector("#first");
+    const paragraph = document.querySelector("#ruud"); 
+    el.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtTEgit69CLM0EzgmkkxxDZ8UE7vbfpG9HTw&s";
+    paragraph.textContent = "";
+    }
+   
+   function secondPack() {
+    const el = document.querySelector("#second");
+    const paragraph = document.querySelector("#son"); 
+    el.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0cs_JHO3buu9_IvE9Ly7z3gCqejoSUrnimw&s";
+    paragraph.textContent = "손흥민";
+    }
+   
+   function secondBack() {
+    const el = document.querySelector("#second");
+    const paragraph = document.querySelector("#son"); 
+    el.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThuTDAoazi3U-ij0bC4HGJ83jWOjwlt5JGQg&s";
+    paragraph.textContent = "";
+    }
+   
+    function thirdPack() {
+    const el = document.querySelector("#third");
+    const paragraph = document.querySelector("#king"); 
+    el.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXjhPEiLTlT50y5GVgstP9XsP5FpckbMHtVA&s";
+    paragraph.textContent = "Ronaldo R9";
+    }
+   
+    function thirdBack() {
+    const el = document.querySelector("#third");
+    const paragraph = document.querySelector("#king"); 
+    el.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSONewK7uLLJnUSadNvPcv3dTaskClEbqtsQ&s";
+    paragraph.textContent = "";
+    }
